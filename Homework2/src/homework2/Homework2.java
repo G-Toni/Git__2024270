017df914133708f6dab5b7d3890bfb265599b6fa
G-Toni <130407061+G-Toni@users.noreply.github.com>
2025-01-20 16:36:33 +0000

    Homework

---
+++ Homework2/src/homework2/Homework2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package homework2;
+
+/**
+ *
+ * @author User
+ */
+public class Homework2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String s = new String("Hi this is Student Gerson - 2024270 and this is going to be an incredible year!");
+        
+    }
+    
+}
